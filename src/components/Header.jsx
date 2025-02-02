1a6577b93e23d203ce4b3890a30bad412a03e7d0
Raymundo Chavez <raymundo.developer@gmail.com>
2025-02-01 23:13:03 -0600

    Mejora estilos de header en movil

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,18 +5,23 @@ const Header = ({ logout }) => {
     <header className="bg-black text-white py-2">
       <div className="mx-8 my-4 md:mx-10 md:my:5 flex justify-between">
         <a href="/">
-          <h1 className="font-bold text-2xl">King of Flat</h1>
+          <h1 className="font-bold md:text-2xl">King of Flat</h1>
         </a>
 
         <ul>
           <li>
-            <Link to={"/matches"}>Enfrentamientos</Link>
+            <Link
+              to={"/matches"}
+              className="text-xs md:text-base font-bold text-blue-500 underline"
+            >
+              Enfrentamientos
+            </Link>
           </li>
         </ul>
 
         <button
           onClick={logout}
-          className="bg-gray-400 text-gray-800 rounded-md px-1 cursor-pointer hover:scale-105 transition-transform will-change-transform"
+          className="bg-gray-400 text-gray-800 rounded-md md:px-1 px-0.5 cursor-pointer hover:scale-105 transition-transform will-change-transform text-xs md:text-base "
         >
           Cerrar sesión
         </button>
--- src/components/players/PlayersBoard.jsx
+++ src/components/players/PlayersBoard.jsx
@@ -99,68 +99,74 @@ const PlayersBoard = () => {
   }
 
   return (
-    <section
-      id="skaters"
-      className="mx-8 my-4 md:mx-10 md:my:5 flex flex-col gap-4 text-center"
-    >
-      <h2 className="text-2xl font-sigmar  ">Patinadores</h2>
-      <div className="grid grid-cols-4 gap-2">
-        {skaters.map((skater) => (
-          <div
-            key={skater.id}
-            onClick={() => handleClick(skater)}
-            className="bg-gray-500 cursor-pointer rounded-lg  capitalize p-2"
-          >
-            {skater.name}
-          </div>
-        ))}
-      </div>
-
-      {isPlayed && (
-        <div>
-          <p className="text-2xl">¿Quién gano?</p>
+    <>
+      <section
+        id="skaters"
+        className="mx-8 my-4 md:mx-10 md:my:5 flex flex-col gap-4 text-center"
+      >
+        <h2 className="text-2xl font-sigmar  ">Patinadores</h2>
+        <div className="grid grid-cols-4 gap-2">
+          {skaters.map((skater) => (
+            <div
+              key={skater.id}
+              onClick={() => handleClick(skater)}
+              className="bg-gray-500 cursor-pointer rounded-lg  capitalize p-2 text-xs md:text-base"
+            >
+              {skater.name}
+            </div>
+          ))}
         </div>
-      )}
-      <div className="flex gap items-center gap-2 justify-center ">
-        <PlayerCard
-          skater={selectedSkaters[0]}
-          isPlayed={isPlayed}
-          setWinner={setWinner}
-        />
-        <span className="text-2xl">VS</span>
-        <PlayerCard
-          skater={selectedSkaters[1]}
-          isPlayed={isPlayed}
-          setWinner={setWinner}
-        />
-      </div>
 
-      <div>
-        <div className="flex flex-col gap-2">
-          <p>
-            Diferencia de SP:{" "}
-            {Math.abs(selectedSkaters[0].points - selectedSkaters[1].points)}
-          </p>
-          <p>
-            Juego por:{" "}
-            {findClosestNumber(
-              Math.abs(selectedSkaters[0].points - selectedSkaters[1].points)
-            )}
-            {" SP"}
-          </p>
+        {isPlayed && (
           <div>
-            {!isPlayed && (
-              <button
-                onClick={() => setIsPlayed(true)}
-                className="bg-green-600 text-white p-2 rounded-xl cursor-pointer"
-              >
-                Jugar
-              </button>
-            )}
+            <p className="text-2xl">¿Quién gano?</p>
+          </div>
+        )}
+        <div className="flex gap items-center gap-2 justify-center ">
+          <PlayerCard
+            skater={selectedSkaters[0]}
+            isPlayed={isPlayed}
+            setWinner={setWinner}
+          />
+          <span className="text-2xl">VS</span>
+          <PlayerCard
+            skater={selectedSkaters[1]}
+            isPlayed={isPlayed}
+            setWinner={setWinner}
+          />
+        </div>
+
+        <div>
+          <div className="flex flex-col gap-2">
+            <p>
+              Diferencia de SP:{" "}
+              {Math.abs(selectedSkaters[0].points - selectedSkaters[1].points)}
+            </p>
+            <p>
+              Juego por:{" "}
+              {findClosestNumber(
+                Math.abs(selectedSkaters[0].points - selectedSkaters[1].points)
+              )}
+              {" SP"}
+            </p>
+            <div>
+              {!isPlayed && (
+                <button
+                  onClick={() => setIsPlayed(true)}
+                  className="bg-green-600 text-white p-2 rounded-xl cursor-pointer"
+                >
+                  Jugar
+                </button>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+        <div className="mt-10 border-t">
+          <h2 className="text-3xl">PlayOffs</h2>
+          <p>Proximamente...</p>
+        </div>
+      </section>
+    </>
   );
 };
 export default PlayersBoard;
